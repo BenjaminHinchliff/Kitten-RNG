b330cd10d845cc8a5bddc7dce068d496e1f7d70a
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-06-24 22:25:21 -0700

    fix errors

--- KRNG/__main__.py
+++ KRNG/__main__.py
@@ -5,7 +5,7 @@ from KRNG import RNGs
 
 def main():
     '''generate a Kitten based random number'''
-    sys.stdout.write(RNGs.KRN())
+    sys.stdout.write(str(RNGs.KRN()))
 
 if __name__ == "__main__":
     main()
